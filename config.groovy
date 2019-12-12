5b0adc2c03e01b495916c63633119d6cc3df75e7
Matthias Fuchs <matthias.fuchs@kps.com>
2019-12-12 20:14:38 +0100

    update compiler config

--- config.groovy
+++ config.groovy
@@ -2,6 +2,13 @@ import groovy.transform.CompileStatic
 import groovy.transform.TypeChecked
 
 withConfig(configuration) {
-	ast(TypeChecked)
-	ast(CompileStatic)
+
+	// inject compile static annotation into every class except for test classes
+	source(classValidator: { ClassNode cn -> !cn.name.endsWith("Test") && !cn.name.endsWith("IT") }) {
+		ast(CompileStatic)
+		ast(TypeChecked)
+	}
+	
+	//enable new groovy 3/java 11 syntax
+	//	configuration.pluginFactory = ParserPluginFactory.antlr4(configuration)
 }
